1c3ee6ea1c1ced57b8f974d8008dad7629e754b6
tuftscs <tuftscs@tuftscsvm.tuftscsvm>
2017-04-11 01:39:54 -0400

    worked more on game in phaser

--- phaser_phun/js/main.js
+++ phaser_phun/js/main.js
@@ -1,17 +1,47 @@
-var game = new Phaser.Game(720, 720, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(1080, 720, Phaser.AUTO, 'light-bikes');
 
-function preload() {
-	game.load.image('player','res/player.png');
-}
+var LightBikes = function (light-bikes) {
+	this.map = null
+	this.layer = null;
+	this.car = null;
 
-function create() {
-	players = game.add.group();
-	createPlayer();
-}
+	this.safetile = 1;
+	this.gridsize = 32;
 
-function update() {
-}
+	this.speed = 150;
+	this.threshold = 3;
+	this.turnSpeed = 100;
 
-function createPlayer(){
-	var player = players.create(0,0, 'player');
-}
+	this.marker = new Phaser.Point();
+	this.turnPoint = new Phaser.Point();
+
+	this.directions = [null, null, null, null, null];
+	this.opposites = [Phaser.NONE, Phaser.RIGHT, Phaser.LEFT, Phaser.DOWN, Phaser.UP];
+
+	this.current = Phaser.UP;
+	this.turning = Phaser.NONE;
+};
+
+LightBikes.prototype = {
+
+	init: function () {
+		this.physics.startSystem(Phaser.Physics.ARCADE);
+	},
+
+	preload: function() {
+		this.load.crossOrigin = 'anonymous';
+		game.load.image('player','res/player.png');
+		game.load.tilemap('map', 'res/Board1.json', null, Phaser.Tilemap.TILED_JSON);
+	},
+
+	create: function() {
+		
+	}
+
+	function update() {
+	}
+
+	function createPlayer(){
+		var player = players.create(0,0, 'player');
+	}
+};
