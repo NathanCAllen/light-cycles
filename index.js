e97048fe1d04725212389e7b42f9636713c14f28
jbfeldman <yohan.feldman@gmail.com>
2017-04-16 21:42:48 -0400

    bug fixes for login, added API for stats retrieval

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var app = express();
-var defualt_ELO = 1200; // subject to change
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+
+var default_ELO = 1200; // subject to change
+var rooms = ["room1", "room2", "room3", "room4"];
+
+
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static('public'))
+var path = require('path');
+
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 
 var bodyParser = require('body-parser'); 
@@ -21,32 +33,46 @@ var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
 	db = databaseConnection;
 });
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
+//
+app.get("/stats", function(request, response){
+	db.collection("players", function(error, coll){
+			if (error){
+	 			console.log("Error: " + error);
+				response.send(500);
+	 		}
+			coll.find().toArray(function(error, items){
+				if (error){
+	 				console.log("Error: " + error);
+					response.send(500);
+	 			}
+				response.send( JSON.stringify(items));
+			});
+		});
+
+});
 
 app.post("/register", function(request, response){
 	//may want to beef up the security on this
-	var username = resquest.body.username;
+	var username = request.body.username;
 	var password = request.body.password;
 	var new_player = {
 		"username" : username,
 		"password": password,
 		"wins": 0,
 		"losses": 0,
-		"ELO": defualt_ELO, //Default ELO subject to change
-		"record": [];
+		"ELO": default_ELO, //Default ELO subject to change
+		"record": []
 	};
 
-	db.collection("player", function(error, coll){
+	db.collection("players", function(error, coll){
 		//check if username already takn
-		db.collection.findOne({"username":username}, function(error, item){
-			if (Object.keys(item).length != 0){
+		coll.findOne({"username":username}, function(error, item){
+			if (item != null){
 				response.send("username already taken");
 			}
 			else{
-				db.collection.insert(new_player, function(error, update){
+				coll.insert(new_player, function(error, update){
 					response.send("succesfully registered!");
 				});
 
@@ -64,12 +90,13 @@ app.post("/login", function(request, response){
 			response.send(500);
 		}
 		else{
-			db.collection.findOne({"username": username, "password": password}, function(error, item){
+			coll.findOne({"username": username, "password": password}, function(error, item){
 				if (item == null){
 					//change this later to what we'll actually send
 					response.send("account not found");
 				}
 				else{
+					//check for password
 					response.send("valid account")
 				}
 			});
@@ -77,15 +104,54 @@ app.post("/login", function(request, response){
 	});
 });
 
-
 app.get('/', function(request, response) {
-//	response.set('Content-Type', 'text/html');
-	respond.send("index.html")
+    response.sendFile(path.join(__dirname + "/public/" + "index.html"));
+
 });
 
 
 
+/*
+io.on('connection',function(socket){
 
+    socket.on('newplayer',function(data){
+    	var new_player = {
+    		"id" : data.username,
+    		"opp": "";
+    		//x, y locations needs to be fixed
+    		"x": 0,
+    		"y": 0,
+    		"socket": socket
+    	}
+    	//search for opponenets
+		for ( i = 0; i < players.length; i++){
+			if (players[i].opp = ""){
+				new_player.opp = players[i];
+				players[i].opp = new_player;
+			}
+		}
+		players.push(new_player);
+        socket.player = new_player;
+        if (new_player.opp != ""){
+        	socket.emit('opponenet', socket,.player.opp);
+
+        	new_player.opp.socket.emit('found_player',socket.player); //change this to broadcast only to opponent
+    	}
+
+        socket.on('click',function(data){
+            console.log('click to '+data.x+', '+data.y);
+            socket.player.x = data.x;
+            socket.player.y = data.y;
+            io.emit('move',socket.player);
+        });
+
+        socket.on('disconnect',function(){
+            io.emit('remove',socket.player.id);
+        });
+    });
+});
+
+*/
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
