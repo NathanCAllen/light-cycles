cfc66453ce10026d78fdf2eb1dbd2e03ea898468
jbfeldman <yohan.feldman@gmail.com>
2017-05-02 12:01:28 -0400

    added a new rom

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ function insert_room(arr, room){
 	arr.push(room);
 }
 
-var empty_rooms = ["Room 0", "Room 1","Room 2", "Room 3", "Room 4", "Room 5", "Room 6", "Room 7", "Room 8", "Room 9", "Room 10"];
+var empty_rooms = ["Room 0", "Room 1","Room 2", "Room 3", "Room 4", "Room 5", "Room 6", "Room 7", "Room 8", "Room 9", "Room 10", "Room 11"];
 var waiting_rooms = [];
 var full_rooms = [];
 io.on('connection',function(socket){
